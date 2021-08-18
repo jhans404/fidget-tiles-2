a06c40164ab7bfa84e838700ac747452d3ada990
Joshua Hans <mrhansmath@gmail.com>
2021-08-18 19:22:17 +0000

    3 working tiles

--- src/Components/ButtonInput.js
+++ src/Components/ButtonInput.js
@@ -2,9 +2,39 @@ import React, {Component} from 'react';
 
 class ButtonInput extends Component {
   // Add a constructor with a state
+  constructor(props) {
+    super(props);
+    this.state = {
+      button: null
+    }
+  };
+
+  // The event will be passed in automatically.
+  handleChange = (event) => {
+    this.setState({"button": event.target.value});
+  }
 
   // Add a render function which returns JSX
-  
+  render() {
+    return(
+      <li className="cards-item">
+        <div className="card">
+          <div className="card-content">
+            <div className="card-title">Date</div>
+            <p className="card-text">
+              <div className="input btn-block">
+                <input type="date" className="textInput" onChange={this.handleChange}/>
+              </div>
+              <div className="output">
+                <label for="dateOutput">State: </label>
+                <span name="dateOutput">{this.state.date}</span>
+              </div>
+            </p>
+          </div>
+        </div>
+      </li>
+    );
+  }
 }
 
 export default ButtonInput;
